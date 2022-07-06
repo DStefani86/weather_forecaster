5e17f09a311ee9a7ed426d8e2f9d751d287a732a
Mona Lisa <stefanibusiness@gmail.com>
2022-07-05 23:22:24 -0700

    got moment working in my JS and added an event listener to the search format

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,4 @@
-// var oneCall =
-//   "https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=%27+" +
-//   cityLat +
-//   "&lon=" +
-//   cityLong +
-//   "&dt=" +
-//   currentTimeUni +
-//   "&units=imperial&appid=" +
-//   APIkey;
-
+var currentTime = moment().format("X");
 var searchInput = document.getElementById("search-input");
 console.log(searchInput);
 var buttonInput = document.getElementById("search-button");
@@ -31,8 +22,39 @@ function searchValue(citySearch) {
     })
     .then(function (data) {
       console.log(data);
-      var lattitude = data.[0].lat
-      var longitude = data.[0].lon
-      var oneCallAPI = 'https://api.openweathermap.org/data/2.5/onecall/timemachine?lat='
+      var lattitude = data[0].lat;
+      var longitude = data[0].lon;
+      var oneCallAPI =
+        "https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=" +
+        lattitude +
+        "&lon=" +
+        longitude +
+        "&appid=" +
+        APIkey +
+        "&dt=" +
+        currentTime;
+      fetch(oneCallAPI)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+        });
     });
+}
+
+{
+  /* <div class="card shadow-0 border">
+              <div class="card-body p-4">
+                <h4 class="mb-1 sfw-normal">City:</h4>
+                <p class="mb-2">Current temperature: <strong>3.644</strong></p>
+                <p>Feels like: <strong>4.37°C</strong></p>
+                <p>Max: <strong>5.325</strong>, Min: <strong>6.3653</strong></p>
+
+                <div class="d-flex flex-row align-items-center">
+                  <p class="mb-0 me-4">Scattered Clouds</p>
+                  <i class="fas fa-cloud fa-3x" style="color: #eee"></i>
+                </div>
+              </div>
+            </div> */
 }
